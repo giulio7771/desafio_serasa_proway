f21426b9e71f7f1dfe3bb87cbce68f169de1ac1a
giovanellawesy <giovanella.wesy@gmail.com>
2020-06-16 15:21:20 -0300

    init

---
+++ app/Http/Controllers/ContabilidadeEmpresaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContabilidadeEmpresaController extends Controller
+{
+    public function atualizaMovimentacoes(Request $request) {
+
+    }
+}
